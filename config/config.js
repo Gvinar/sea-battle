a6b52b5dc0833d15163bb1a00a5be1b0b9c71089
Andrey Glazunov <Gvinar@yandex.ru>
2016-02-24 19:26:43 +0300

    Init Database

--- config/config.js
+++ config/config.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let format = require('fomatto').Formatter();
+
+if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = "production";
+}
+require("./appDefaultEnv_" + process.env.NODE_ENV);
+
+let config = {
+    database: {
+        connectionUrl: format(process.env.DB_URL, { username: process.env.DB_USERNAME, password: process.env.DB_PASSWORD, dbName: process.env.DB_NAME })
+    }
+};
+
+module.exports = config;
