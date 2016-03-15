17c595d4e48fedda6614f262a2493fabb314827d
Ilya <dalechinilya@gmail.com>
2016-03-15 19:42:30 +0300

    no message

--- client/scripts/start.component.js
+++ client/scripts/start.component.js
@@ -2,13 +2,7 @@
     app.AppStart =
         ng.core.Component({
             selector: 'start-app',
-            template:
-            '<div class="start">' +
-            '<button class="start__button start__button_big">Start Game</button>' +
-            '<button class="start__button">Sign In</button>' +
-            "<button [routerLink]="['/sigh_in']" class='start__button'>Sign Up</button>" +
-            "<p class='start__text'>If you won't login, you can't save the game!</p>" +
-            '</div>'
+            templateURl:../views/start.html
         })
             .Class({
                 constructor: function() {}
